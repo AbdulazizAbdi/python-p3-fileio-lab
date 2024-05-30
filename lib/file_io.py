4649c8f18b205168f6511f344bf667b3a1c371c2
Abdulaziz Abdi <amigf76@gmail.com>
2024-05-30 12:48:49 +0300

    fileio lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode = 'w', encoding = 'utf-8') as written_file:
+        written_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode = 'a', encoding = 'utf-8') as appended_file:
+        appended_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', mode = 'r', encoding = 'utf-8') as readin_file:
+        return readin_file.read()
